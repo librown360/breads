05d7a196bbb9f09887a0c64ccae3f6861ae7e4df
Lisa Brown <lisaann493@verizon.net>
2022-02-12 18:13:23 -0500

    sending index and show data

---
+++ controllers/breads_controller.js
@@ -0,0 +1,16 @@
+// DEPENDENCIES
+const express = require('express')
+const breads = express.Router()
+const Bread = require('../models/bread')
+
+// INDEX
+breads.get('/', (req, res) => {
+    res.send(Bread)
+})
+
+// SHOW
+breads.get('/:arrayIndex', (req, res) => {
+    res.send(Bread[req.params.arrayIndex])
+})
+
+module.exports = breads
